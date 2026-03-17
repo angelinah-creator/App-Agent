b8a6dcb6aaa388420187ce437027b112db3aa3d8
Kevinfal383 <kevinfal383@gmail.com>
2026-03-17 10:01:03 +0300

    Ajout des favicons

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,6 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </head>
       <body className={`${montserrat.variable} font-sans`}>
         <QueryProvider>
           {children}
